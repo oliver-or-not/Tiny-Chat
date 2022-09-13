e40c4fd44874aa94938995ce974831384e236d15
oliver-wonil <stellaria700@gmail.com>
2022-09-13 21:50:19 +0900

    register and log in are connected to Firebase

--- Tiny Chat/Controller/LogInViewController.swift
+++ Tiny Chat/Controller/LogInViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class LogInViewController: UIViewController {
 
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 	}
 	
+	@IBAction func LogInPressed(_ sender: UIButton) {
+		if let email = emailTextField.text, let password = passwordTextField.text {
+			Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+				if let e = error {
+					print(e.localizedDescription)
+				} else {
+					self.performSegue(withIdentifier: "LogInToChat", sender: self)
+				}
+			}
+		}
+	}
 	
 }
--- Tiny Chat/Controller/RegisterViewController.swift
+++ Tiny Chat/Controller/RegisterViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 	}
 	
+	@IBAction func registerPressed(_ sender: UIButton) {
+		
+		if let email = emailTextField.text, let password = passwordTextField.text {
+			Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+				if let e = error {
+					print(e.localizedDescription)
+				} else {
+					self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+				}
+			}
+		}
+	}
 	
 }
